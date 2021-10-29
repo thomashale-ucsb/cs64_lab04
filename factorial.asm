056d016953f7a49d9124626634be49907a8aa32d
thomashale-ucsb <thomashale@ucsb.edu>
2021-10-28 23:29:15 -0700

    factorial rought draft

--- factorial.asm
+++ factorial.asm
@@ -30,6 +30,65 @@
 main:
     
 	# TODO: Write your code here
+    
+    #printing out the questioning statement
+    li $v0 4
+    la $a0 askNum
+    syscall
+
+    # get num
+    li $v0 5
+    syscall
+    add $t0 $v0 $0
+
+#looping stuff lol
+factLoop:
+    
+    #if (n < 0) --> stop looping
+    ble $t0 1 exitStuff
+    
+    #action stuff
+    mult $t0 $t1
+    mflo $t1
+    
+    #decrement schizz
+    add $t0 -1
+
+    j factLoop
+
+#handles the final ugly string thingy
+exitStuff:
+    
+    #typing all of this is painful
+    
+    #shapirz
+    li $v0 4
+    la $a0 shapirz
+    syscall
+
+    #num being factorialed
+    li $v0 1
+    la $a0 $t0
+    syscall
+    
+    #is
+    li $v0 4
+    la $a0 is
+    syscall
+
+    #factz and logicz (factorial number)
+    li $v0 1
+    la $a0 $t1
+    syscall
+
+    #endln
+    li $v0 4
+    la $a0 endln
+    syscall
+    
+
+    #why not?
+    j exit
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
