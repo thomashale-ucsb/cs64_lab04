bb2822ebe9bb95ca788487913c1f0f6668a403e3
thomashale-ucsb <thomashale@ucsb.edu>
2021-10-28 22:24:03 -0700

    init vars

--- factorial.asm
+++ factorial.asm
@@ -13,12 +13,25 @@
 .data
 
 	# TODO: Write your initializations here
+    # kk teacher
+    askNum: .asciiz "Enter a number:\n"
+    shapirz: .asciiz "Factorial of "
+    is: .asciiz "is:\n"
+    endln: .asciiz "\n"
+
+    #numbs and anaesthesia
+    li $t0 0        #loop count stuff
+    li $t1 1        #factorialz and logic stuff
 
 #Text Area (i.e. instructions)
 .text
+    # loop with bge (bge >= 0), till increment counter is < 0, then goto
+    # end processing label thingy, which then gotos exit
 main:
-
+    
 	# TODO: Write your code here
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
+    li $v0 10
+    syscall
